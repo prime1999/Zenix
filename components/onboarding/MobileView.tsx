ce0b5821488c54d87245a760349b0cacd47c1ce9
Prime <primetechnologies02@gmail.com>
2026-07-24 01:58:14 +0100

    added the isComplete to the mobile onboarding drawer

--- components/onboarding/MobileView.tsx
+++ components/onboarding/MobileView.tsx
@@ -1,14 +1,7 @@
 "use client";
 
-import { useState } from "react";
-import {
-  Drawer,
-  DrawerClose,
-  DrawerContent,
-  DrawerHeader,
-  DrawerTitle,
-  DrawerFooter,
-} from "@/components/ui/drawer";
+import { useState, useEffect } from "react";
+import { Drawer, DrawerContent } from "@/components/ui/drawer";
 import {
   Tooltip,
   TooltipContent,
@@ -16,9 +9,18 @@ import {
 } from "@/components/ui/tooltip";
 import { PanelRightOpen, X } from "lucide-react";
 import RealtimeInterface from "./RealtimeInterface";
+import { useOnboardingStore } from "@/stores/onboardingStore";
 
 const MobileView = () => {
   const [open, setOpen] = useState(false);
+
+  const isComplete = useOnboardingStore((state) => state.isComplete);
+
+  useEffect(() => {
+    if (isComplete) {
+      setOpen(true);
+    }
+  }, [isComplete]);
 
   return (
     <div className="md:hidden">
@@ -26,9 +28,12 @@ const MobileView = () => {
         <TooltipTrigger
           asChild
           className="absolute top-7 right-5 cursor-pointer bg-primary-purple text-white rounded-full p-3 shadow-md hover:opacity-90 transition-opacity"
-          onClick={() => setOpen(true)}
         >
-          <button type="button" aria-label="Open Zenix Insights">
+          <button
+            type="button"
+            onClick={() => setOpen(true)}
+            aria-label="Open Zenix Insights"
+          >
             <PanelRightOpen className="w-5 h-5" />
           </button>
         </TooltipTrigger>
@@ -37,18 +42,16 @@ const MobileView = () => {
         </TooltipContent>
       </Tooltip>
 
-      <Drawer open={open} onOpenChange={setOpen} direction="right">
+      <Drawer
+        open={open || isComplete}
+        onOpenChange={setOpen}
+        direction="right"
+      >
         <DrawerContent className="fixed inset-y-0 right-0 z-50 h-full w-[85vw] max-w-sm flex flex-col rounded-l-xl rounded-r-none backdrop-blur-3xl bg-transparent">
           <div>
             {" "}
             <RealtimeInterface />
           </div>
-
-          {/* <DrawerFooter className="pt-2 border-t px-4 pb-4">
-            <DrawerClose className="w-full py-2 bg-secondary text-secondary-foreground rounded-md text-sm font-medium hover:bg-secondary/80 transition-colors">
-              continue
-            </DrawerClose>
-          </DrawerFooter> */}
         </DrawerContent>
       </Drawer>
     </div>
